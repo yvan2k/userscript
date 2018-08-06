aea49743538a154e88d2c9f5d056f21fa38faa0a
yvan2k <yvan2000@gmail.com>
2018-08-06 23:16:51 +0500

    Update tvmaze_calendar.user.js

--- tvmaze_calendar.user.js
+++ tvmaze_calendar.user.js
@@ -1,77 +1,70 @@
 // ==UserScript==
 // @name        Streamline tvmaze calendar
 // @namespace   tvmaze
-// @include     http://www.tvmaze.com/calendar*
+// @include     /^https?:\/\/www\.tvmaze\.com\/calendar/
 // @downloadURL https://github.com/yvan2k/userscript/blob/master/tvmaze_calendar.user.js?raw=true
 // @updateURL   https://github.com/yvan2k/userscript/blob/master/tvmaze_calendar.user.js?raw=true
 // @version     1
 // @grant       none
 // ==/UserScript==
 
+map = new Map([
+       ["", "entry "],
+       ["0", "entry watched"],
+       ["1", "entry acquired"],
+       ["2", "entry skipped"]]) ;
+
 $(".entry a").each(function () {
-  var episode_id, show_id ;
   try {
-    episode_id = this.href.match(/(?:episodes\/)([0-9]+)/)[1] ;
+    var episode_id = this.href.match(/(?:episodes\/)([0-9]+)/)[1] ;
+    var show_id = this.href.match(/(?:shows\/)([0-9]+)/)[1] ;
   } catch (e){
   };
 
-  try {
-    show_id = this.href.match(/(?:shows\/)([0-9]+)/)[1] ;
-  } catch (e){
-  };
-  
-  if (show_id != undefined) {
-    console.log(show_id, this, $(this).parent()[0].attributes["class"]) ;      
-    var str0 = "<input class=\"postcheckbox\" type=\"checkbox\" checked show_id=\"" + show_id + "\" >" ;
-    $(this).before(str0) ;    
-  }
-  
+  if (show_id != undefined)
+    $(this).before("<input class=\"postcheckbox\" type=\"checkbox\" checked show_id=\"" + show_id + "\" >") ;    
+
   if (episode_id != undefined) {
-    if (/x01$/.test($(this).text())) {
-//      console.log(episode_id, this, $(this).parent()[0].attributes["class"]) ;      
-      var style = "background-color: " + (/^1x/.test($(this).text()) ? "plum" : "lightpink") + ";" /*+ "font-size: 14px; font-weight: bold;"*/ ;      
-      $($($(this).parent()[0]).find("a")).attr("style", style) ;
-    }
+    if (/x01$/.test($(this).text()))
+      $($($(this).parent()[0]).find("a")).attr("style", "background-color: " + (/^1x/.test($(this).text()) ? "plum" : "lightpink") + ";" /*+ "font-size: 14px; font-weight: bold;"*/) ;
 
-    _class = $(this).parent()[0].attributes["class"].value ;
-    if      (_class == "entry watched")  type = "0"
-    else if (_class == "entry acquired") type = "1"
-    else                                 type = "" ;
-    
+    for (var [key, value] of map)
+      if ($($(this).parent()[0]).parent()[0].attributes["class"].value == value) {
+        var type = key ;
+        break ;
+      }
+
     var str = "<br>" ;
-    ["", "0", "1", "2"].forEach(function (e, i, a) {
+    
+    for (var key of map.keys()) {
        str = str + 
        "<input class=\"postradiobutton\" type=\"radio\""
           + " style=\"margin-bottom: 0px;\""
-          + " value=\"" + e + "\""
+          + " value=\"" + key + "\""
           + " episode_id=\"" + episode_id + "\""
           + " name=rb" + episode_id + ""
-          + ((type == e) ? " checked=\"true\"" : "")
+          + ((type == key) ? " checked=\"true\"" : "")
           + " />"
-    }) ;
+    }
+  
     $(this).after(str) ;
   }
 })
 
 $(document).on('change', '.postradiobutton', function(e){
-  var _class = "" ;
-
-       if (this.value == "")  _class = "entry"
-  else if (this.value == "0") _class = "entry watched"
-  else if (this.value == "1") _class = "entry acquired"
-  else if (this.value == "2") _class = "entry skipped" ;
-
-  $.post('/watch/set?episode_id=' + this.attributes['episode_id'].value, {type: this.value}) ;
-
-  $($(this).parent()[0]).toggleClass() ;
-  $($(this).parent()[0]).toggleClass(_class) ;
+  var rb = $($($(this).parent()[0]).parent()[0]), val = this.value ;
+  $.post(
+    '/watch/set?episode_id=' + this.attributes['episode_id'].value,
+    {type: this.value},
+    function() {rb.toggleClass().toggleClass(map.get(val))}
+  ) ;
 });
 
 $(document).on('change', '.postcheckbox', function(e){
   $.post('/follow/toggle?show_id=' + this.attributes['show_id'].value + '&widget=small', {}) ;
 });
 
-$("li .entry").css("padding-bottom", "0") ;
-$("li .entry").css("padding-top", "0") ;
+$(".calendar .entry").css("padding-bottom", "0px").css("padding-top", "0px") ;
 $(".postcheckbox").css("margin-bottom", "0") ;
 $(".row").css("max-width", "120rem") ;
+$(".ad-margin-left").remove() ;
