fe74fd6e26f14c6ba40320bc7b16bc5125377af4
yvan2k <yvan2000@gmail.com>
2018-08-18 12:19:12 +0500

    Update tvmaze_calendar.user.js

--- tvmaze_calendar.user.js
+++ tvmaze_calendar.user.js
@@ -4,52 +4,48 @@
 // @include     /^https?:\/\/www\.tvmaze\.com\/calendar/
 // @downloadURL https://github.com/yvan2k/userscript/blob/master/tvmaze_calendar.user.js?raw=true
 // @updateURL   https://github.com/yvan2k/userscript/blob/master/tvmaze_calendar.user.js?raw=true
-// @version     1
+// @version     2
 // @grant       none
 // ==/UserScript==
 
 map = new Map([
-       ["", "entry "],
-       ["0", "entry watched"],
-       ["1", "entry acquired"],
-       ["2", "entry skipped"]]) ;
+        ["",  "entry "],
+        ["1", "entry acquired"],
+        ["0", "entry watched"],
+        ["2", "entry skipped"]
+      ]) ;
 
-$(".entry a").each(function () {
-  try {
-    var episode_id = this.href.match(/(?:episodes\/)([0-9]+)/)[1] ;
-    var show_id = this.href.match(/(?:shows\/)([0-9]+)/)[1] ;
-  } catch (e){
-  };
+$(".show-wrap").each(function () {
+  var links = $(this).find("a") ;
+  var show_id = links[0].href.split("\/")[4] ;
+  var episode_id = links[1].href.split("\/")[4] ;
+  $(links[0]).before("<input class=\"postcheckbox\" type=\"checkbox\" checked show_id=\"" + show_id + "\" >") ;
+  var str = "" ;
 
-  if (show_id != undefined)
-    $(this).before("<input class=\"postcheckbox\" type=\"checkbox\" checked show_id=\"" + show_id + "\" >") ;    
+  if (/x01$/.test($(links[1]).text()))
+    $(links[0]).attr("style", "background-color: " + (/^1x/.test($(links[1]).text()) ? "plum" : "lightpink") + ";" /*+ "font-size: 14px; font-weight: bold;"*/) ;
 
-  if (episode_id != undefined) {
-    if (/x01$/.test($(this).text()))
-      $($($(this).parent()[0]).find("a")).attr("style", "background-color: " + (/^1x/.test($(this).text()) ? "plum" : "lightpink") + ";" /*+ "font-size: 14px; font-weight: bold;"*/) ;
-
-    for (var [key, value] of map)
-      if ($($(this).parent()[0]).parent()[0].attributes["class"].value == value) {
-        var type = key ;
-        break ;
-      }
-
-    var str = "<br>" ;
-    
-    for (var key of map.keys()) {
-       str = str + 
-       "<input class=\"postradiobutton\" type=\"radio\""
-          + " style=\"margin-bottom: 0px;\""
-          + " value=\"" + key + "\""
-          + " episode_id=\"" + episode_id + "\""
-          + " name=rb" + episode_id + ""
-          + ((type == key) ? " checked=\"true\"" : "")
-          + " />"
+  for (var [key, value] of map)
+    if ($(this).parent()[0].attributes["class"].value == value) {
+      var type = key ;
+      console.log(type) ;
+      break ;
     }
-  
-    $(this).after(str) ;
+
+  for (var key of map.keys()) {
+     str = str + 
+     "<input class=\"postradiobutton\" type=\"radio\""
+        + " style=\"margin-bottom: 0px;\""
+        + " value=\"" + key + "\""
+        + " episode_id=\"" + episode_id + "\""
+        + " name=rb" + episode_id + ""
+        + ((type == key) ? " checked=\"true\"" : "")
+        + " />"
   }
+
+  $(this).prepend(str) ;
 })
+
 
 $(document).on('change', '.postradiobutton', function(e){
   var rb = $($($(this).parent()[0]).parent()[0]), val = this.value ;
@@ -68,3 +64,4 @@ $(".calendar .entry").css("padding-bottom", "0px").css("padding-top", "0px") ;
 $(".postcheckbox").css("margin-bottom", "0") ;
 $(".row").css("max-width", "120rem") ;
 $(".ad-margin-left").remove() ;
+$(".cinereus").parent()[1].remove() ;
